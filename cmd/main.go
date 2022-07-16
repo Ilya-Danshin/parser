ea6caef2fcacaf4f2bc8c92c483f34637e7939f8
Ilya-Danshin <iljadanshin@gmail.com>
2022-07-16 11:07:01 +0300

    added Db init

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"Parser/config"
+	"Parser/database"
 	"log"
 )
 
@@ -12,6 +13,12 @@ func main() {
 	}
 
 	settings, err := config.ReadConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db := database.NewDBService()
+	err = db.Init(settings.DB)
 	if err != nil {
 		log.Fatal(err)
 	}
