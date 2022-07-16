d8d3f9d6eaca0c9bb11f250eb70e86c02362113b
Ilya-Danshin <iljadanshin@gmail.com>
2022-07-16 10:52:09 +0300

    added config

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"Parser/config"
+	"log"
+)
+
 func main() {
+	err := config.InitConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	settings, err := config.ReadConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 }
